221ed1d58a7885d4db1c07c017ba1042b8e8adda
ahmadrosid <alahmadrosid@gmail.com>
2022-07-02 07:35:27 +0700

    feat: solution for problem 03

--- main.ml
+++ main.ml
@@ -5,13 +5,20 @@ let rec last (xs: 'a list): 'a option =
 		| [x] -> Some x
 		| _ :: rest -> last rest
 
-(* Problem 2 *)
+(* Problem 02 *)
 let rec last_two (xs: 'a list): ('a * 'a) option =
 	match xs with
 		| [] -> None
 		| [_] -> None
 		| [x; y] -> Some (x, y)
 		| _ :: rest -> last_two rest
+
+(* Problem 03 *)
+let rec at (k: int) (xs: 'a list): 'a option =
+	match xs with
+		| x :: rest when k == 1 -> Some x
+		| _ :: rest when k > 1 -> at (k - 1) rest
+		| _ -> None
 
 let () = 
 	print_endline "Hello world"
